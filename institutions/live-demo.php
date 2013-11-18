b0c6c5931e437324fea6ae34efc3d25c60bba1f6
Chris <chris.sammarco@gmail.com>
2013-11-18 16:35:41 -0500

    removed breadcrumb navbar

--- institutions/live-demo.php
+++ institutions/live-demo.php
@@ -1,27 +1,6 @@
 <?php include '../views/header.php'; ?>
         <!-- Content Start -->
         <div id="main">
-	    <!-- Title, Breadcrumb Start-->
-            <div class="breadcrumb-wrapper">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-lg-6 col-md-6 col-xs-12 col-sm-6">
-                        <h2 class="title">Live Demo</h2>
-                     </div>
-                     <div class="col-lg-6 col-md-6 col-xs-12 col-sm-6">
-                        <div class="breadcrumbs pull-right">
-                           <ul>
-                              <li>You are here:</li>
-                              <li><a href="../index.html">Home</a></li>
-                              <li>Institutions</li>
-                              <li>Live Demo</li>
-                           </ul>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-             </div>
-          <!-- Title, Breadcrumb End-->
           <!-- Main Content start-->
             <div class="content">
                <div class="container">
