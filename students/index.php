49367398f3cdaa1a32488e55b43618c94ea67bc3
Chris <chris.sammarco@gmail.com>
2013-11-18 12:29:12 -0500

    updated student lp, added detect script

--- students/index.php
+++ students/index.php
@@ -2,7 +2,7 @@
         <!-- Content Start -->
         <div id="main">
 	    <!-- Title, Breadcrumb Start-->
-            <div class="breadcrumb-wrapper">
+<!--            <div class="breadcrumb-wrapper">
                <div class="container">
                   <div class="row">
                      <div class="col-lg-6 col-md-6 col-xs-12 col-sm-6">
@@ -19,16 +19,70 @@
                      </div>
                   </div>
                </div>
-             </div>
+             </div>-->
           <!-- Title, Breadcrumb End-->
           <!-- Main Content start-->
-            <div class="main-content" style="">
+            <div class="content">
                <div class="container">
-                   <div class="row">
-
-                   </div>
+                  <div class="row">
+                     <!-- Skill Section Start -->
+                     <div class="choice col-lg-3 col-md-4 col-sm-4 col-xs-12 ">
+                        <h3 class="title">ExamHawk Info</h3>
+                        <ul class="check-list why">
+                           <li>Lorem Ipsum</li>
+                           <li>Lorem Ipsum</li>
+                           <li>Lorem Ipsum</li>
+                        </ul>
+                        <br>
+                        <h3 class="title">Your Computer</h3>
+                        <ul class="check-list why">
+                           <li id="user-os">Operating System: </li>
+                           <li id="user-browser">Browser Type: </li>
+                           <li id="user-version">Browser Version: </li>
+                        </ul>
+                        <br>
+                        <a class="btn-special btn-new-light-blue" href="#" style="font-size:16px;color:#FFFFFF;">Download</a>
+                     </div>
+                     <!-- Skill Section Start -->
+                     <!-- Right Section Start -->
+                     <div class="posts-block col-lg-9 col-md-8 col-sm-8 col-xs-12">
+                        <article>
+                           <h2 class="student-header">Easy, Convenient, Cost Effective</h2>
+                           <div class="post-content">
+                              <div class="img-content">
+                                 <img src="<?php echo BASE_URL; ?>/img/student-test.jpg" alt="">
+                              </div>
+                              <p style="text-align: justify;">
+                                 There are many variations of passages of <a href="#">Lorem Ipsum</a> available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly <a href="#">believable</a>. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic <a href="#">words etc.</a>
+                              </p>
+                           </div>
+                        </article>
+                     </div>
+                     <!-- Right Section End -->
+                  </div>
                </div>
             </div>
+            <!-- Slogan Start-->
+            <div class="slogan bottom-pad-small">
+               <div class="container">
+                  <div class="row">
+                     <div class="slogan-content">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                           <h3>Having Trouble? Call us today at <b>(123)456-7890</b> or Email us at <a href="mailto:info@examhawk.com">info@examhawk.com</a></h3>
+                        </div>
+<!--                        <div class="col-lg-3 col-md-3">
+                           <div class="get-started">
+                              <button class="btn btn-special btn-color pull-right">Contact Us</button>
+                           </div>
+                        </div>-->
+                        <div class="clearfix"></div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- Slogan End-->
+            <div class="divider"></div>
+         
             <!-- Main Content end-->
          </div>
          <!-- Content End -->
