b01a046577794e1586da4be0e8f01df7e6b7943d
Chris <chris.sammarco@gmail.com>
2013-11-21 18:03:17 -0500

    updated copy on institution and front page

--- institutions/index.php
+++ institutions/index.php
@@ -13,36 +13,35 @@
 							<img src="http://placehold.it/300" alt="">
 						</div>
 						<p style="text-align:justify;">
-							 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-						</p>
+                            For any online education program, balancing security of online tests vs. convenience, cost and technological sophistication is always a balancing act.  ExamHawk is an online exam monitoring service for institutions looking for a flexible, technically-robust remote exam monitoring solution. The service’s mission is to increase access to online exams, making the experience convenient, cost effective and easy for both the educator and test-taker while maintaining the highest exam integrity.
+                        </p>
+                        <p>
+                            Have more questions? Check out our FAQs, or Product Tour
+                        </p>
 					</div>
 					</article>
 				</div>
 				<!-- Left Section End -->
 				<!-- Skill Section Start -->
 				<div class="choice col-lg-6 col-md-6 col-sm-6 col-xs-12">
-					<h3 class="title">How It Works</h3>
 					<div class="step">
-						<span>1</span>
-						<h2>Step One</h2>
+						<h2>Simple</h2>
 						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							Engaging and setting up ExamHawk for your institution is painless, just give us a call! Our testing system is LMS and test delivery system agnostic, making instructor set up straightforward.
 						</p>
 						<img src="http://placehold.it/120" alt="step 1"/>
 					</div>
 					<div class="step">
-						<span>2</span>
-						<h2>Step Two</h2>
+						<h2>Secure</h2>
 						<p>
-							Curabitur laoreet urna vel ante molestie facilisis.
+							Every test session is monitored by a live person and all webcam, audio and screen data is recorded and stored.
 						</p>
 						<img src="http://placehold.it/120" alt="step 2"/>
 					</div>
 					<div class="step">
-						<span>3</span>
-						<h2>Step Three</h2>
+						<h2>Compliant</h2>
 						<p>
-							Aliquam sem eros, sodales et sapien ac, dignissim suscipit nunc.
+							Our authentication & test security system  is key in proving an online program is reliable and top level. All data is stored under the FERPA and Safe Harbor regulations.
 						</p>
 						<img src="http://placehold.it/120" alt="step 3"/>
 					</div>
@@ -51,49 +50,63 @@
 			</div>
 		</div>
 	</div>
-	<div class="divider">
-	</div>
+	<!-- Slogan Start-->
+            <div class="slogan">
+               <div class="container">
+                  <div class="row">
+                     <div class="slogan-content">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                           <h3>Having Trouble? Call us today at <b>(123)456-7890</b> or Email us at <a href="mailto:info@examhawk.com">info@examhawk.com</a></h3>
+                        </div>
+<!--                        <div class="col-lg-3 col-md-3">
+                           <div class="get-started">
+                              <button class="btn btn-special btn-color pull-right">Contact Us</button>
+                           </div>
+                        </div>-->
+                        <div class="clearfix"></div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- Slogan End-->
 	<!-- 3 Column Services -->
 	<div class="service-reasons">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<!--				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					<h3 class="title">Three Reasons ExamHawk is Awesome</h3>
 				</div>
 				<div class="divider">
-				</div>
+				</div>-->
 				<div class="reasons">
 					<div class="col-lg-4 col-md-4 col-sm-4 animate_afc d1">
 						<div class="content-box big ch-item bottom-pad-small">
 							<div class="content-box-info">
-								<h3>Reason One</h3>
+								<h3>Quote One</h3>
 								<p>
 									 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 								</p>
-								<a href="#">Read More <i class="icon-angle-right"></i><i class="icon-angle-right"></i></a>
-							</div>
+                            </div>
 						</div>
 					</div>
 					<div class="col-lg-4 col-md-4 col-sm-4 animate_afc d2">
 						<div class="content-box big ch-item bottom-pad-small">
 							<div class="content-box-info">
-								<h3>Reason Two</h3>
+								<h3>Quote Two</h3>
 								<p>
 									 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 								</p>
-								<a href="#">Read More <i class="icon-angle-right"></i><i class="icon-angle-right"></i></a>
-							</div>
+                            </div>
 						</div>
 					</div>
 					<div class="col-lg-4 col-md-4 col-sm-4 animate_afc d3">
 						<div class="content-box big ch-item">
 							<div class="content-box-info">
-								<h3>Reason Three</h3>
+								<h3>Quote Three</h3>
 								<p>
 									 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 								</p>
-								<a href="#">Read More <i class="icon-angle-right"></i><i class="icon-angle-right"></i></a>
-							</div>
+                            </div>
 						</div>
 					</div>
 				</div>
@@ -135,20 +148,17 @@
 					<div class="clearfix">
 					</div>
 				</div>
-				<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-					<h3 class="title">Contact Us for a Live demo</h3>
-				</div>
+
 				<div class="posts-block col-lg-4 col-md-4 col-sm-4 col-xs-12">
-					<article>
+                    <h3 class="title">Header</h3>
 					<div class="post-content">
 						<p style="text-align:justify;">
 							 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
 						</p>
 					</div>
-					</article>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-					<!--                        <h3 class="title">Send Us an Email</h3>-->
+					<h3 class="title">Contact Us for a Live Demo</h3>
 					<form method="post" class="reply" id="contact">
 						<fieldset>
 							<div class="row">
--- students/index.php
+++ students/index.php
@@ -20,19 +20,19 @@
                            <li id="user-version">Browser Version: </li>
                         </ul>
                         <br>
-                        <a class="btn-special btn-new-light-blue" href="#" style="font-size:16px;color:#FFFFFF;">Download</a>
+                        <a class="btn-special btn-new-light-blue" href="#" style="font-size:12px;color:#FFFFFF;">Download ExamHawk</a>
                      </div>
                      <!-- Skill Section Start -->
                      <!-- Right Section Start -->
                      <div class="posts-block col-lg-9 col-md-8 col-sm-8 col-xs-12">
                         <article>
-                           <h2 class="student-header">Easy, Convenient, Cost Effective</h2>
+                           <h2 class="student-header">Easy and Convenient</h2>
                            <div class="post-content">
                               <div class="img-content">
                                  <img src="<?php echo BASE_URL; ?>/img/student-test.jpg" alt="">
                               </div>
                               <p style="text-align: justify;">
-                                 There are many variations of passages of <a href="#">Lorem Ipsum</a> available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly <a href="#">believable</a>. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic <a href="#">words etc.</a>
+                                  Everything you need to take your next online test is in the Exam Application application (click the big blue button to the left!) With ExamHawk, you can take your test anywhere, anytime – all you need is a working webcam and internet connection, and to be in a quiet room. We are always available to help. Interested in how the product works? Check out our product page.  For more information about technical requirements, the experience and company, check out our FAQs.
                               </p>
                            </div>
                         </article>
