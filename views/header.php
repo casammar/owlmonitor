13895cda0de7658002bd7e1628799241cbcee7e0
Chris <chris.sammarco@gmail.com>
2013-11-22 16:05:45 -0500

    header links should point to test-takers

--- views/header.php
+++ views/header.php
@@ -81,7 +81,7 @@
                            <a id="<?php echo ($_SERVER['REQUEST_URI'] == '/beta/institutions/')?'current':''?>" href="<?php echo BASE_URL; ?>/institutions">Institutions</a>
                         </li>
                         <li>
-                           <a id="<?php echo ($_SERVER['REQUEST_URI'] == '/beta/students/')?'current':''?>" href="<?php echo BASE_URL; ?>/students">Test Takers</a>
+                           <a id="<?php echo ($_SERVER['REQUEST_URI'] == '/beta/test-takers/')?'current':''?>" href="<?php echo BASE_URL; ?>/test-takers">Test Takers</a>
                         </li>
                         <li>
                            <a id="<?php echo ($_SERVER['REQUEST_URI'] == '/beta/about/')?'current':''?>" href="<?php echo BASE_URL; ?>/about" class="sf-with-ul">About</a>
