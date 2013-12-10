e92495f0a0812d67c42cb926499581b95dc37cc7
Chris <chris.sammarco@gmail.com>
2013-12-10 10:53:26 -0500

    adding instructor page shell

---
+++ instructors/index.php
@@ -0,0 +1,51 @@
+<?php include '../views/header.php'; ?>
+<!-- Content Start -->
+<div id="main">
+	<!-- Main Content start-->
+	<div class="content">
+		<div class="container">
+			<div class="row">
+				<div class="posts-block col-lg-6 col-md-6 col-sm-6 col-xs-12">
+					<article>
+					<h3 class="title">Instructors</h3>
+					<div class="post-content">
+						<div class="img-content">
+							<img src="../img/sandbox/college-doorway.jpg" alt="">
+						</div>
+						<p style="text-align:justify;">
+                            For any online education program, balancing security of online tests vs. convenience, cost and technological sophistication is always a balancing act.  ExamHawk is an online exam monitoring service for institutions looking for a flexible, technically-robust remote exam monitoring solution. The service’s mission is to increase access to online exams, making the experience convenient, cost effective and easy for both the educator and test-taker while maintaining the highest exam integrity.
+                        </p>
+                        <p>
+                            Have more questions? Check out our FAQs, or Product Tour
+                        </p>
+					</div>
+					</article>
+				</div>
+				<div class="choice col-lg-6 col-md-6 col-sm-6 col-xs-12">
+					<div class="step">
+						<h3 class="title">Simple</h3>
+						<p>
+                            <i class="icon-laptop icon-5x" style="float:left;margin:5px 15px;height:87px;width:87px;"></i> Engaging and setting up ExamHawk for your institution is painless, just give us a call! Our testing system is LMS and test delivery system agnostic, making instructor set up straightforward.
+                            <br><br>
+                        </p>
+					</div>
+					<div class="step">
+						<h3 class="title">Secure</h3>
+						<p>
+							<i class="icon-shield icon-5x" style="float:left;margin:5px 15px;height:87px;width:87px;"></i> Every test session is monitored by a live person and all webcam, audio and screen data is recorded and stored.
+                            <br><br>
+                        </p>
+					</div>
+					<div class="step">
+						<h3 class="title">Compliant</h3>
+						<p>
+							<i class="icon-ok icon-5x" style="float:left;margin:5px 15px;height:87px;width:87px;"></i> Our authentication & test security system  is key in proving an online program is reliable and top level. All data is stored under the FERPA and Safe Harbor regulations.
+                            <br><br>
+                        </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include '../views/footer.php'; ?>
