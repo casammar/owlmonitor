81c394ba6c8f6010d42d5722009e57dd82b38d0b
Chris <chris.sammarco@gmail.com>
2013-12-13 16:14:39 -0500

    removed read more buttons from news page

--- about/news.php
+++ about/news.php
@@ -36,9 +36,6 @@
                                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen not only five centuries, but also the leap essentially.
                               </p>
                            </div>
-                           <footer class="post-footer">
-                              <a class="btn-small btn-color">Read More</a>
-                           </footer>
                         </article>
                         <div class="blog-divider"></div>
                         <article class="post hentry">
@@ -71,9 +68,6 @@
                                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen not only five centuries, but also the leap essentially.
                               </p>
                            </div>
-                           <footer class="post-footer">
-                              <a class="btn-small btn-color">Read More</a>
-                           </footer>
                         </article>
                         <div class="blog-divider"></div>
                         <article class="post hentry">
@@ -106,9 +100,6 @@
                                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen not only five centuries, but also the leap essentially.
                               </p>
                            </div>
-                           <footer class="post-footer">
-                              <a class="btn-small btn-color">Read More</a>
-                           </footer>
                         </article>
                         <div class="blog-divider"></div>
                         <div class="pagination-centered">
