12c24133070cc4557156e407030621d341a2f152
Chris <chris.sammarco@gmail.com>
2013-12-16 10:36:54 -0500

    fixed error by renaming functions in color switcher

--- js/switcher.js
+++ js/switcher.js
@@ -1,6 +1,6 @@
 
 /* ================================================================= */
-/* CSS Style Switcher By FIFO THEMES
+/* CSS Style Switcher
 ====================================================================== */
 
 window.console = window.console || (function(){
@@ -11,173 +11,58 @@ window.console = window.console || (function(){
 
 jQuery(document).ready(function($) {
 
-		// Color Changer
+    $("#style-switcher h2 a").click(function(e){
+	    e.preventDefault();
+	    var div = $("#style-switcher");
+	    console.log(div.css("left"));
+	    if (div.css("left") === "-206px") {
+		    $("#style-switcher").animate({
+			    left: "0px"
+		    });
+	    } else {
+		    $("#style-switcher").animate({
+			    left: "-206px"
+		    });
+	    }
+    });
 
-		$(".blue" ).click(function(){
+    //Header Color Change
+    $(".header-bg").spectrum({
+	    showInitial: true,
+	    color: "#323B44",
+	    preferredFormat: "hex6",
+	    showInput: true,
+	    move: updateHeader
+    });
+    //Changing the header color instantly
+    function updateHeader(color) {
+	$("#header .main-header").css("background", color.toHexString());
+    }
 
-			$("#background-banner" ).css("background-color", "#FFFFFF");
-			return false;
-		});
+    //Banner Color Change
+    $(".banner-bg").spectrum({
+	    showInitial: true,
+	    color: "#BFCDF1",
+	    preferredFormat: "hex6",
+	    showInput: true,
+	    move: updateBanner
+    });
+    //Changing the banner color instantly
+    function updateBanner(color) {
+	$("#background-banner").css("background", color.toHexString());
+    }
 
-		$(".green" ).click(function(){
-			$("#colors" ).attr("href", "css/colors/green.css" );
-			return false;
-		});
+    //Title Color Change
+    $(".title-bg").spectrum({
+	    showInitial: true,
+	    color: "#1D3599",
+	    preferredFormat: "hex6",
+	    showInput: true,
+	    move: updateTitle
+    });
+    //Changing the title color instantly
+    function updateTitle(color) {
+	$("#background-title").css("color", color.toHexString());
+    }
 
-		$(".orange" ).click(function(){
-			$("#colors" ).attr("href", "css/colors/orange.css" );
-			return false;
-		});
-
-
-		$(".purple" ).click(function(){
-			$("#colors" ).attr("href", "css/colors/purple.css" );
-			return false;
-		});
-
-		$(".red" ).click(function(){
-			$("#colors" ).attr("href", "css/colors/red.css" );
-			return false;
-		});
-
-		$(".magenta" ).click(function(){
-			$("#colors" ).attr("href", "css/colors/magenta.css" );
-			return false;
-		});
-
-
-		$(".brown" ).click(function(){
-			$("#colors" ).attr("href", "css/colors/brown.css" );
-			return false;
-		});
-
-		$(".gray" ).click(function(){
-			$("#colors" ).attr("href", "css/colors/gray.css" );
-			return false;
-		});
-
-		$(".teal" ).click(function(){
-			$("#colors" ).attr("href", "css/colors/teal.css" );
-			return false;
-		});
-
-		$(".golden" ).click(function(){
-			$("#colors" ).attr("href", "css/colors/golden.css" );
-			return false;
-		});
-
-
-
-
-
-		$("#style-switcher h2 a").click(function(e){
-			e.preventDefault();
-			var div = $("#style-switcher");
-			console.log(div.css("left"));
-			if (div.css("left") === "-206px") {
-				$("#style-switcher").animate({
-					left: "0px"
-				});
-			} else {
-				$("#style-switcher").animate({
-					left: "-206px"
-				});
-			}
-		});
-
-
-
-//Header Color Change
-$(".header-bg").spectrum({
-    showInitial: true,
-	color: "#323B44",
-	preferredFormat: "hex6",
-    showInput: true,
-	move: updateHeader
 });
-//Changing the header color instantly
-function updateHeader(color) {
-    $("#header .main-header").css("background", color.toHexString());
-}
-
-
-
-//Footer Top Color Change
-$(".banner-bg").spectrum({
-    showInitial: true,
-	color: "#BFCDF1",
-	preferredFormat: "hex6",
-	showInput: true,
-	move: updateFooterTop
-});
-//Changing the header color instantly
-function updateFooterTop(color) {
-    $("#background-banner").css("background", color.toHexString());
-}
-
-//Footer Top Color Change
-$(".title-bg").spectrum({
-    showInitial: true,
-	color: "#1D3599",
-	preferredFormat: "hex6",
-	showInput: true,
-	move: updateFooterTop
-});
-//Changing the header color instantly
-function updateFooterTop(color) {
-    $("#background-banner h3").css("color", color.toHexString());
-}
-
-
-
-
-		// Footer Style Switcher
-	   $("#footer-style").change(function(e){
-			if( $(this).val() == 1){
-				$("#footer").removeClass("dark");
-				$("#footer-bottom").removeClass("dark");
-			} else{
-				$("#footer").addClass("dark");
-				$("#footer-bottom").addClass("dark");
-			}
-		});
-
-		//Layout Switcher
-	   $("#layout-style").change(function(e){
-			if( $(this).val() == 1){
-				$("body").removeClass("boxed"),
-				$(window).resize();
-			} else{
-				$("body").addClass("boxed"),
-				$(window).resize();
-			}
-		});
-
-		$("#layout-switcher").on('change', function() {
-			$('#layout').attr('href', $(this).val() + '.css');
-		});
-
-		$(".colors li a").click(function(e){
-			e.preventDefault();
-			$(this).parent().parent().find("a").removeClass("active");
-			$(this).addClass("active");
-		});
-
-		$('.bg li a').click(function() {
-			var current = $('#style-switcher select[id=layout-style]').find('option:selected').val();
-			if(current == '2') {
-				var bg = $(this).css("backgroundImage");
-				$("body").css("backgroundImage",bg);
-			} else {
-				alert('Please select boxed layout');
-			}
-		});
-
-		$("#reset a").click(function(e){
-			var bg = $(this).css("backgroundImage");
-			$("body").css("backgroundImage","url(./images/bg/noise.png)");
-			$("#navigation" ).removeClass("style-2")
-		});
-
-
-	});
