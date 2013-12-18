948a1db1d1164fb16fb5f5bafeb2fcd3a83dc961
Chris <chris.sammarco@gmail.com>
2013-12-18 11:23:54 -0500

    updating institution page with new layout, removed sandbox pages

--- institutions/index.php
+++ institutions/index.php
@@ -1,216 +1,84 @@
 <?php include '../views/header.php'; ?>
 <!-- Content Start -->
-<div id="main">
-	<!-- Main Content start-->
-	<div class="content">
-		<div class="container">
-			<div class="row">
-				<div class="posts-block col-lg-6 col-md-6 col-sm-6 col-xs-12">
-					<article>
-					<h3 class="title">Institutions</h3>
-					<div class="post-content">
-						<div class="img-content">
-							<img src="../img/sandbox/college-doorway.jpg" alt="">
-						</div>
-						<p style="text-align:justify;">
-                            For any online education program, balancing security of online tests vs. convenience, cost and technological sophistication is always a balancing act.  ExamHawk is an online exam monitoring service for institutions looking for a flexible, technically-robust remote exam monitoring solution. The service’s mission is to increase access to online exams, making the experience convenient, cost effective and easy for both the educator and test-taker while maintaining the highest exam integrity.
-                        </p>
-                        <p>
-                            Have more questions? Check out our FAQs, or Product Tour
-                        </p>
-					</div>
-					</article>
-				</div>
-				<!-- Left Section End -->
-				<!-- Skill Section Start -->
-				<div class="choice col-lg-6 col-md-6 col-sm-6 col-xs-12">
-					<div class="step">
-						<h3 class="title">Simple</h3>
-						<p>
-                            <i class="icon-laptop icon-5x" style="float:left;margin:5px 15px;height:87px;width:87px;"></i> Engaging and setting up ExamHawk for your institution is painless, just give us a call! Our testing system is LMS and test delivery system agnostic, making instructor set up straightforward.
-                            <br><br>
-                        </p>
-
-
-					</div>
-					<div class="step">
-						<h3 class="title">Secure</h3>
-						<p>
-							<i class="icon-shield icon-5x" style="float:left;margin:5px 15px;height:87px;width:87px;"></i> Every test session is monitored by a live person and all webcam, audio and screen data is recorded and stored.
-                            <br><br>
-                        </p>
-
-					</div>
-					<div class="step">
-						<h3 class="title">Compliant</h3>
-						<p>
-							<i class="icon-ok icon-5x" style="float:left;margin:5px 15px;height:87px;width:87px;"></i> Our authentication & test security system  is key in proving an online program is reliable and top level. All data is stored under the FERPA and Safe Harbor regulations.
-                            <br><br>
-                        </p>
-
-					</div>
-				</div>
-
-                <!-- Skill Section Start -->
-			</div>
-             <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-                        <br>
-                        <a class="scrolldown" href="#quotes" style="cursor: pointer; display: inline;text-decoration:none;"><i class="icon-chevron-down icon-3x"></i></a>
+<div id="main" class="institution-background-alt">
+    <div class="container">
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <h2><small>Exam Monitoring for </small><br> Institutions</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <dl>
+                    <dt>Simple</dt>
+                    <dd>Engaging and setting up ExamHawk for your institution is painless, just give us a call! Our testing system is LMS and test delivery system agnostic, making instructor set up straightforward.</dd>
+                    <dt>Secure</dt>
+                    <dd>Every test session is monitored by a live person and all webcam, audio and screen data is recorded and stored. </dd>
+                    <dt>Compliant</dt>
+                    <dd>Our authentication & test security system is key in proving an online program is reliable and top level. All data is stored under the FERPA and Safe Harbor regulations. </dd>
+                </dl>
+            </div>
+            <div class="col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-lg-4 col-md-4 col-sm-4 col-xs-12" id="contact-form" style="background-color:#FCFCFC;opacity:0.8;">
+                <h2 class="title">Request a Live Demo</h2>
+                <p>
+                    <strong>Let us show you how the platform works</strong>
+                </p>
+                <form method="post" class="reply" id="contact">
+                <fieldset>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <label>Name: <span>*</span></label>
+                            <input class="form-control" id="name" name="name" type="text" value="" required>
+                        </div>
                     </div>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <label>Email: <span>*</span></label>
+                            <input class="form-control" type="email" id="email" name="email" value="" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <label>Phone: <span>*</span></label>
+                            <input class="form-control" type="text" id="phone" name="phone" value="" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <label>Company: <span>*</span></label>
+                            <input class="form-control" type="text" id="company" name="company" value="" required>
+                        </div>
+                    </div>
+                </fieldset>
+                <button class="btn-normal btn-color submit bottom-pad pull-right" type="submit">Send</button>
+                <div class="success alert-success alert" style="display:none">
+                    Your message has been sent successfully.
                 </div>
-		</div>
-	</div>
-	<!-- 3 Column Services -->
-	<div class="service-reasons" id="quotes">
-		<div class="container">
-			<div class="row">
-<!--				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-					<h3 class="title">Three Reasons ExamHawk is Awesome</h3>
-				</div>
-				<div class="divider">
-				</div>-->
-				<div class="reasons" >
-					<div class="col-lg-4 col-md-4 col-sm-4">
-						<div class="content-box big ch-item bottom-pad-small" style="width:90%;">
-							<div class="content-box-info client-quote">
-								<blockquote>
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                  <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
-                                </blockquote>
-                            </div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-4">
-						<div class="content-box big ch-item bottom-pad-small" style="width:90%;">
-							<div class="content-box-info client-quote">
-								<blockquote>
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                  <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
-                                </blockquote>
-                            </div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-4">
-						<div class="content-box big ch-item" style="width:90%;">
-							<div class="content-box-info client-quote">
-								<blockquote>
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                  <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
-                                </blockquote>
-                            </div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- 3 Column Big Services -->
-	<div class="services-big">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
-					<!-- pricing table -->
-					<div class="p_table">
-						<!-- column style 1 -->
-						<div class="column_1" style="width:100%;">
-							<ul>
-								<!-- column header -->
-								<li class="header_row_1 align_center">
-								<h2 class="col1">ExamHawk Pricing</h2>
-								</li>
-                            </ul>
-                            <table class="table table-striped">
-                             <thead>
-                               <tr>
-                                 <th>Exam Length</th>
-                                 <th>Per Test</th>
-                               </tr>
-                             </thead>
-                             <tbody>
-                               <tr>
-                                 <td>120 Minutes or Less</td>
-                                 <td>$10.00</td>
-                               </tr>
-                               <tr>
-                                 <td>120-180 Minutes</td>
-                                 <td>$15.00</td>
-                               </tr>
-                               <tr>
-                                 <td>181 Minutes or More</td>
-                                 <td>$20.00</td>
-                               </tr>
-                               <tr>
-                                   <td>&nbsp;</td>
-                                   <td>&nbsp;</td>
-                               </tr>
-                             </tbody>
-                             <thead>
-                               <tr>
-                                 <th>Optional Fees (can be assessed to the student)</th>
-                               </tr>
-                             </thead>
-                              <tbody>
-                               <tr>
-                                 <td>Take it Now (on-demand)</td>
-                                 <td>$8.75</td>
-                               </tr>
-                               <tr>
-                                 <td>Take it Soon (within 72 hours of a test)</td>
-                                 <td>$5.00</td>
-                               </tr>
-                             </tbody>
-                           </table>
-						</div>
-					</div>
-					<div class="clearfix">
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-4 col-sm-4 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-12">
-					<h3 class="title">Contact Us for a Live Demo</h3>
-					<form method="post" class="reply" id="contact">
-						<fieldset>
-							<div class="row">
-								<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-									<label>Name: <span>*</span></label>
-									<input class="form-control" id="name" name="name" type="text" value="" required>
-								</div>
-                            </div>
-                            <div class="row">
-								<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-									<label>Email: <span>*</span></label>
-									<input class="form-control" type="email" id="email" name="email" value="" required>
-								</div>
-							</div>
-                            <div class="row">
-								<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-									<label>Phone: <span>*</span></label>
-									<input class="form-control" type="text" id="phone" name="phone" value="" required>
-								</div>
-							</div>
-                            <div class="row">
-								<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-									<label>Company: <span>*</span></label>
-									<input class="form-control" type="text" id="company" name="company" value="" required>
-								</div>
-							</div>
-						</fieldset>
-						<button class="btn-normal btn-color submit pull-right" type="submit">Send</button>
-						<div class="success alert-success alert" style="display:none">
-							Your message has been sent successfully.
-						</div>
-						<div class="error alert-error alert" style="display:none">
-							E-mail must be valid and message must be longer than 100 characters.
-						</div>
-
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- Main Content end-->
+                <div class="error alert-error alert" style="display:none">
+                    E-mail must be valid and message must be longer than 100 characters.
+                </div>
+                <div class="clearfix">
+                </div>
+            </form>
+             </div>
+        </div>
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-center">
+                <button type="button" class="btn btn-warning btn-lg">Live Demo</button>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <blockquote class="pull-right">
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."</p>
+                    <small>John Smith - <cite title="Source Title">Professor</cite></small>
+                </blockquote>
+            </div>
+        </div>
+    </div>
 </div>
-<!-- Content End -->
+<!-- Main End -->
 <?php include '../views/footer.php'; ?>
