96eb84a28d0914a9385dcadbe6d51d917bb42bd8
Chris <chris.sammarco@gmail.com>
2013-12-20 16:05:03 -0500

    styling header and titles to match logo colors

--- institutions/index.php
+++ institutions/index.php
@@ -1,25 +1,126 @@
-<?php include '../views/header.php'; ?>
+<?php include '../views/baseurl.php'; ?>
+<!DOCTYPE html>
+<!--[if IE 8]>			<html class="ie ie8"> <![endif]-->
+<!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->	<html> <!--<![endif]-->
+   <head>
+      <meta charset="utf-8">
+      <title>ExamHawk</title>
+      <meta name="description" content="ExamHawk application">
+      <meta name="author" content="ExamHawk development team">
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Google Fonts -->
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Chango' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:400,700,800,800italic,400italic,700italic' rel='stylesheet' type='text/css'>
+      <!-- Library CSS -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/bootstrap.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/fonts/font-awesome/css/font-awesome.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/animations.css" media="screen">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/superfish.css" media="screen">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/team-member.css" media="screen">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/revolution-slider/css/settings.css" media="screen">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/prettyPhoto.css" media="screen">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/pricing-table.css" media="screen">
+      <!-- Theme CSS -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/style.css">
+      <!-- Skin -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/colors/blue.css">
+      <!-- Steps -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/steps.css">
+      <!-- Schedule Grid -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/schedule-grid.css">
+      <!-- Responsive CSS -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/theme-responsive.css">
+      <!-- Institutions-->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/institutions.css">
+
+      <!-- Favicons -->
+      <link rel="shortcut icon" href="<?php echo BASE_URL; ?>/img/wise-owl-transparent.png">
+      <link rel="apple-touch-icon" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-72.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-114.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-144.png">
+      <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+      <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/respond.min.js"></script>
+      <![endif]-->
+      <!--[if IE]>
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/ie.css">
+      <![endif]-->
+   </head>
+   <body class="home">
+      <div class="wrap">
+
 <!-- Content Start -->
 <div id="main" class="institution-background-alt">
     <div class="container">
+                  <!-- TopNav Start -->
+                  <div class="topnav navbar-header">
+                     <a class="navbar-toggle down-button" data-toggle="collapse" data-target=".slidedown">
+                     <i class="icon-angle-down icon-current"></i>
+                     </a>
+                  </div>
+                  <!-- TopNav End -->
+                  <!-- Logo Start -->
+                  <div class="logo pull-left">
+		      <a href="<?php echo BASE_URL; ?>">
+			  <img src="../img/005-examhawk-logo.png" alt="ExamHawk">
+		      </a>
+                  </div>
+		  <!-- Logo End -->
+                  <!-- Mobile Menu Start -->
+                  <div class="mobile navbar-header">
+                     <a class="navbar-toggle" data-toggle="collapse" href=".navbar-collapse">
+                     <i class="icon-reorder icon-2x"></i>
+                     </a>
+                  </div>
+                  <!-- Mobile Menu End -->
+		  <!-- Menu Start -->
+                  <nav class="collapse navbar-collapse menu">
+                     <ul class="nav navbar-nav sf-menu">
+                        <li>
+                           <a href="<?php echo BASE_URL; ?>/institutions" class="" style="color:#00BDFF;">Institutions</a>
+                        </li>
+			<li>
+                           <a href="<?php echo BASE_URL; ?>/test-takers" class="" >Test Takers</a>
+                        </li>
+			<li>
+                           <a href="<?php echo BASE_URL; ?>/about" class="">About</a>
+                        </li>
+			<li>
+                           <a href="<?php echo BASE_URL; ?>/institutions/live-demo.php" class="demo-link">Live Demo</a>
+                        </li>
+		     </ul>
+		  </nav>
+		  <!-- Menu End -->
+               </div>
+    <div class="container">
         <div class="divider"></div>
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <h2><small>Exam Monitoring for </small><br> Institutions</h2>
-            </div>
+            <div class="col-lg-offset-6 col-md-offset-6 col-sm-offset-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <h2 style="color:#1D3599;"><small style="color:#00BDFF;">Online Exam Monitoring for </small><br> Institutions</h2>
+             </div>
         </div>
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="col-lg-offset-6 col-md-offset-6 col-sm-offset-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
                 <dl>
-                    <dt>Simple</dt>
+                    <dt style="color:#00BDFF;text-align: center;">Simple</dt>
                     <dd>Engaging and setting up ExamHawk for your institution is painless, just give us a call! Our testing system is LMS and test delivery system agnostic, making instructor set up straightforward.</dd>
-                    <dt>Secure</dt>
+                    <dt style="color:#00BDFF;text-align: center;">Secure</dt>
                     <dd>Every test session is monitored by a live person and all webcam, audio and screen data is recorded and stored. </dd>
-                    <dt>Compliant</dt>
+                    <dt style="color:#00BDFF;text-align: center;">Compliant</dt>
                     <dd>Our authentication & test security system is key in proving an online program is reliable and top level. All data is stored under the FERPA and Safe Harbor regulations. </dd>
                 </dl>
             </div>
-            <div class="col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-lg-4 col-md-4 col-sm-4 col-xs-12" id="contact-form" style="background-color:#FCFCFC;opacity:0.8;">
+        </div>
+<!--         <div class="divider"></div>
+       <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-center">
                 <h2 class="title">Request a Live Demo</h2>
                 <p>
                     <strong>Let us show you how the platform works</strong>
@@ -61,15 +162,9 @@
                 <div class="clearfix">
                 </div>
             </form>
-             </div>
-        </div>
-        <div class="divider"></div>
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-center">
-                <button type="button" class="btn btn-warning btn-lg">Live Demo</button>
             </div>
-        </div>
-        <div class="divider"></div>
+        </div>-->
+<!--        <div class="divider"></div>
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                 <blockquote class="pull-right">
@@ -77,7 +172,7 @@
                     <small>John Smith - <cite title="Source Title">Professor</cite></small>
                 </blockquote>
             </div>
-        </div>
+        </div>-->
     </div>
 </div>
 <!-- Main End -->
