2fe40fa332ee187f78a0af4340136794a69e4872
Chris <chris.sammarco@gmail.com>
2013-12-30 17:04:50 -0500

    adding favicon and apple touch icon to front page

--- index.php
+++ index.php
@@ -34,11 +34,17 @@
       <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/spectrum.css">
       <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/slider.css">
       <!-- Favicons -->
-      <link rel="shortcut icon" href="<?php echo BASE_URL; ?>/img/wise-owl-transparent.png">
+      <link rel="shortcut icon" href="<?php echo BASE_URL; ?>/img/ico/favicon.ico" type="image/x-icon">
+      <link rel="icon" href="<?php echo BASE_URL; ?>/img/ico/favicon.ico" type="image/x-icon">
       <link rel="apple-touch-icon" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon.png">
+      <link rel="apple-touch-icon" sizes="57x57" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-60x60.png" />
       <link rel="apple-touch-icon" sizes="72x72" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-72.png">
+      <link rel="apple-touch-icon" sizes="76x76" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-76x76.png" />
       <link rel="apple-touch-icon" sizes="114x114" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-114.png">
+      <link rel="apple-touch-icon" sizes="120x120" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-120x120.png" />
       <link rel="apple-touch-icon" sizes="144x144" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-144.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-152x152.png" />
       <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
       <!--[if lt IE 9]>
       <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
