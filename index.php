c0935fffd788acb973ec5b9b9127db9f883d7413
Chris <chris.sammarco@gmail.com>
2013-12-30 14:05:56 -0500

    changed front page extenstion to php. made background full screen on load

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
+<?php include 'views/baseurl.php'; ?>
 <!DOCTYPE html>
 <!--[if IE 8]>			<html class="ie ie8"> <![endif]-->
 <!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
-<!--[if gt IE 9]><!-->	<html lang="en"> <!--<![endif]-->
+<!--[if gt IE 9]><!-->	<html> <!--<![endif]-->
    <head>
       <meta charset="utf-8">
       <title>ExamHawk</title>
@@ -15,28 +16,29 @@
       <link href='http://fonts.googleapis.com/css?family=Chango' rel='stylesheet' type='text/css'>
       <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
       <!-- Library CSS -->
-      <link rel="stylesheet" href="css/bootstrap.css">
-      <link rel="stylesheet" href="css/fonts/font-awesome/css/font-awesome.css">
-      <link rel="stylesheet" href="css/animations.css" media="screen">
-      <link rel="stylesheet" href="css/superfish.css" media="screen">
-      <link rel="stylesheet" href="css/revolution-slider/css/settings.css" media="screen">
-      <link rel="stylesheet" href="css/prettyPhoto.css" media="screen">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/bootstrap.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/fonts/font-awesome/css/font-awesome.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/animations.css" media="screen">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/superfish.css" media="screen">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/revolution-slider/css/settings.css" media="screen">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/prettyPhoto.css" media="screen">
       <!-- Theme CSS -->
-      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/style.css">
       <!-- Skin -->
-      <link rel="stylesheet" href="css/colors/blue.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/colors/blue.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/institutions.css">
       <!-- Responsive CSS -->
-      <link rel="stylesheet" href="css/theme-responsive.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/theme-responsive.css">
       <!-- Switcher CSS -->
-      <link rel="stylesheet" href="css/switcher.css">
-      <link rel="stylesheet" href="css/spectrum.css">
-      <link rel="stylesheet" href="css/slider.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/switcher.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/spectrum.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/slider.css">
       <!-- Favicons -->
-      <link rel="shortcut icon" href="img/wise-owl-transparent.png">
-      <link rel="apple-touch-icon" href="img/ico/apple-touch-icon.png">
-      <link rel="apple-touch-icon" sizes="72x72" href="img/ico/apple-touch-icon-72.png">
-      <link rel="apple-touch-icon" sizes="114x114" href="img/ico/apple-touch-icon-114.png">
-      <link rel="apple-touch-icon" sizes="144x144" href="img/ico/apple-touch-icon-144.png">
+      <link rel="shortcut icon" href="<?php echo BASE_URL; ?>/img/wise-owl-transparent.png">
+      <link rel="apple-touch-icon" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-72.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-114.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-144.png">
       <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
       <!--[if lt IE 9]>
       <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -46,8 +48,10 @@
       <link rel="stylesheet" href="css/ie.css">
       <![endif]-->
    </head>
-   <body class="home">
-      <div class="wrap">
+   <body >
+
+       <div class="background-banner">
+
          <!-- Header Start -->
 
          <!-- Header End -->
@@ -66,10 +70,11 @@
             </div>-->
 	 <!-- mobile CTA's end -->
          <!-- Content Start -->
-         <div id="main">
+
 	    <!-- background image row start -->
-	    <div class="row background-banner">
-		<div class="container">
+        <div class="container">
+	    <div class="row">
+
                   <!-- TopNav Start -->
                   <div class="topnav navbar-header">
                      <a class="navbar-toggle down-button" data-toggle="collapse" data-target=".slidedown">
@@ -79,8 +84,8 @@
                   <!-- TopNav End -->
                   <!-- Logo Start -->
                   <div class="logo pull-left">
-		      <a href="index.html">
-			  <img src="img/004-examhawk-logo.png" alt="ExamHawk">
+		      <a href="<?php echo BASE_URL; ?>/index.html">
+			  <img src="<?php echo BASE_URL; ?>/img/005-examhawk-logo.png" alt="ExamHawk">
 		      </a>
                   </div>
 		  <!-- Logo End -->
@@ -91,31 +96,35 @@
                      </a>
                   </div>
                   <!-- Mobile Menu End -->
-		  <!-- Menu Start -->
+                  <!-- Menu Start -->
                   <nav class="collapse navbar-collapse menu">
                      <ul class="nav navbar-nav sf-menu">
                         <li>
-                           <a href="institutions" class="">Institutions</a>
+                           <a href="<?php echo BASE_URL; ?>/institutions" class="">Institutions</a>
                         </li>
-			<li>
-                           <a href="test-takers" class="">Test Takers</a>
+                        <li>
+                           <a href="<?php echo BASE_URL; ?>/test-takers" class="">Test Takers</a>
                         </li>
-			<li>
-                           <a href="about" class="">About</a>
+                        <li>
+                           <a href="<?php echo BASE_URL; ?>/about" class="">About</a>
                         </li>
-			<li>
-                           <a href="institutions/live-demo.php" class="demo-link">Live Demo</a>
+                        <li>
+                           <a href="<?php echo BASE_URL; ?>/institutions/live-demo.php" class="demo-link">Live Demo</a>
                         </li>
-		     </ul>
-		  </nav>
-		  <!-- Menu End -->
+                    </ul>
+                 </nav>
+                 <!-- Menu End -->
                </div>
-		<div class="container" style="padding-top: 200px;">
-		    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="margin-top:100px;">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bottom-pad">
+			    <img src="img/sandbox/lg-owl.png" style="width:200px;height:250px;" class="pull-left"/>
 			    <h3 id="background-title">Live & Recorded</h3>
 			    <h3 id="background-subtitle">Online Exam Monitoring</h3>
 			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bottom-pad">
+			    <p style="text-align:justify;">ExamHawk is a new online exam monitoring service that allows learners to take exams in a secure online environment. The platform is a simple and cost effective way to prevent cheating and maintain exam integrity. </p>
+			</div>
+
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center">
 				<a class="btn btn-special btn-lg btn-color product-tour" >Learn More</a>
@@ -124,16 +133,13 @@
 				<a class="btn btn-special btn-lg btn-warning" href="institutions/live-demo.php" >Live Demo</a>
 			    </div>
 			</div>
-			 <blockquote class="pull-right" style="color:#2B2B2B;border-right:none;margin-top: 300px;">
-			   <p>"For the value, convenience and technical sophistication, ExamHawk can't be beat."</p>
-			   <small>- Alfred E Neuman, Head of digital learning<br><br><br></small>
-			 </blockquote>
 		    </div>
-		</div>
 	    </div>
 	    <!-- background image row end -->
-	    <!-- Demo Video Start -->
-	    <div class="content" style="background: url('img/cream_dust.png') repeat;">
+
+       </div>
+
+        <div class="content" style="background: url('img/cream_dust.png') repeat;">
 		<div class="container">
 		  <div class="divider"></div>
 		  <div class="row features-row">
@@ -228,41 +234,42 @@
 		    </div>
 		</div>
 	    </div>
-	    <!-- Demo Video End -->
 
-         </div>
          <!-- Content End -->
          <!-- Footer Start -->
          <footer id="footer">
             <!-- Footer Top Start -->
             <div class="footer-top">
                <div class="container contact-us">
-                  <div class="row bottom-pad">
-		     <section class="col-lg-3 col-md-3 col-xs-12 col-sm-3 footer-one">
-			 <a href="institutions"><h3 class="footer-heading">Institutions</h3></a>
-			 <ul>
-			     <li><a href="about/faq.php#institution-faq">Institution FAQs</a></li>
-			     <li><a href="institutions/live-demo.php">Live Demo</a></li>
-			 </ul>
-		     </section>
-		     <section class="col-lg-3 col-md-3 col-xs-12 col-sm-3 footer-two">
-		         <a href="test-takers"><h3>Test Takers</h3></a>
-			 <ul>
-			     <li><a href="about/faq.php#test-takers-faq">Test Taker FAQs</a></li>
-			     <li><a href="test-takers">Download</a></li>
-			 </ul>
-		     </section>
+                  <div class="row">
+                    <section class="col-lg-3 col-md-3 col-xs-12 col-sm-3 footer-one">
+                        <h3>Institutions</h3>
+                    <ul>
+                        <li><a href="<?php echo BASE_URL; ?>/institutions">Pricing</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>/about/faq.php#institution-faq">Institution FAQs</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>/institutions/live-demo.php">Live Demo</a></li>
+                    </ul>
+                    </section>
+                    <section class="col-lg-3 col-md-3 col-xs-12 col-sm-3 footer-two">
+                        <h3>Test Takers</h3>
+                    <ul>
+                        <li><a href="<?php echo BASE_URL; ?>/test-takers">How it Works</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>/about/faq.php#test-takers-faq">Test Taker FAQs</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>/test-takers">Download</a></li>
+                    </ul>
+                    </section>
 
-		      <section class="col-lg-3 col-md-3 col-xs-12 col-sm-3 footer-three">
-		         <a href="about"><h3>About</h3></a>
-			 <ul>
-                             <li><a href="about/team.php">Team</a></li>
-                             <li><a href="about/contact.php">Contact</a></li>
-                             <li><a href="about/conferences.php">Conferences</a></li>
-			     <li><a href="about/blog.php">Blog</a></li>
-			 </ul>
-		     </section>
-		      <section class="col-lg-3 col-md-3 col-xs-12 col-sm-3 footer-three">
+                     <section class="col-lg-3 col-md-3 col-xs-12 col-sm-3 footer-three">
+                        <h3>About</h3>
+                    <ul>
+                        <li><a href="<?php echo BASE_URL; ?>/about/mission.php">Mission / Vision</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>/about/team.php">Team</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>/about/contact.php">Contact</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>/about/conferences.php">Conferences</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>/about/blog.php">Blog</a></li>
+                    </ul>
+                    </section>
+                     <section class="col-lg-3 col-md-3 col-xs-12 col-sm-3 footer-three">
                         <h3>Contact Us</h3>
                         <ul >
                            <li>
@@ -290,15 +297,13 @@
             <div class="footer-bottom">
                <div class="container">
                   <div class="row">
-                     <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 text-center"> &copy; Copyright 2013 by ExamHawk. All Rights Reserved. </div>
+                     <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 text-center"> &copy; Copyright 2013 ExamHawk. All Rights Reserved. </div>
                   </div>
                </div>
             </div>
             <!-- Footer Bottom End -->
          </footer>
-         <!-- Scroll To Top -->
-         <a href="#" class="scrollup"><i class="icon-angle-up"></i></a>
-      </div>
+
       <!-- Wrap End -->
       <section id="style-switcher">
         <h2>Style Switcher <a href="#"><i class="icon-cog"></i></a></h2>
@@ -322,35 +327,29 @@
         </div>
      </section>
       <!-- The Scripts -->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/bootstrap.js"></script>
-      <script src="js/jquery.parallax.js"></script>
-      <script src="js/modernizr-2.6.2.min.js"></script>
-      <script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
-      <script src="js/jquery.nivo.slider.pack.js"></script>
-      <script src="js/jquery.prettyPhoto.js"></script>
-      <script src="js/jquery.isotope.js"></script>
-      <script src="js/superfish.js"></script>
-      <script src="js/tweetMachine.js"></script>
-      <script src="js/tytabs.js"></script>
-      <script src="js/jquery.gmap.min.js"></script>
-      <script src="js/circularnav.js"></script>
-      <script src="js/jquery.sticky.js"></script>
-      <script src="js/jflickrfeed.js"></script>
-      <script src="js/imagesloaded.pkgd.min.js"></script>
-      <script src="js/waypoints.min.js"></script>
-      <script src="js/spectrum.js"></script>
-      <script src="js/switcher.js"></script>
-      <script src="js/bootstrap-slider.js"></script>
-      <script src="js/custom.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/jquery.min.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/bootstrap.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/jquery.parallax.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/modernizr-2.6.2.min.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/jquery.nivo.slider.pack.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/jquery.prettyPhoto.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/jquery.isotope.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/superfish.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/tweetMachine.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/tytabs.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/jquery.gmap.min.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/circularnav.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/jquery.sticky.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/jflickrfeed.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/imagesloaded.pkgd.min.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/waypoints.min.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/spectrum.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/switcher.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/bootstrap-slider.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/custom.js"></script>
       <!--
       <script type='text/javascript'>(function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://www.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({ c: 'd5e7d80c-967b-4390-a12c-c25e4bfd6633', f: true }); done = true; } }; })();</script>
       -->
-      <script>
-	  $(window).bind("resize", function() {
-	     // $(".background-banner").css("height", $(window).height() - 0);
-	      //console.log($(window).height() - 0);
-	  });
-      </script>
    </body>
 </html>
