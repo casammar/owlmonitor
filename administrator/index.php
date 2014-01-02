d50ff972df9584c99e451775f1a486d8661ded10
Chris <chris.sammarco@gmail.com>
2014-01-02 12:11:41 -0500

    adding administrator page to view site metrics. Added SEO stats https://github.com/eyecatchup/SEOstats, MIT liscense

---
+++ administrator/index.php
@@ -0,0 +1,90 @@
+<?php include '../views/baseurl.php'; ?>
+<!DOCTYPE html>
+<!--[if IE 8]>			<html class="ie ie8"> <![endif]-->
+<!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->	<html> <!--<![endif]-->
+   <head>
+      <meta charset="utf-8">
+      <title>ExamHawk | Online Exam Monitoring | Administrator</title>
+      <meta name="description" content="ExamHawk application">
+      <meta name="author" content="ExamHawk development team">
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Google Fonts -->
+      <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
+      <!-- Library CSS -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/bootstrap.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/fonts/font-awesome/css/font-awesome.css">
+      <!-- Theme CSS -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/style.css">
+      <!-- Skin -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/custom.css">
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/institutions.css">
+      <!-- Responsive CSS -->
+      <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/theme-responsive.css">
+      <!-- Favicons -->
+      <link rel="shortcut icon" href="<?php echo BASE_URL; ?>/img/wise-owl-transparent.png">
+      <link rel="apple-touch-icon" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-72.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-114.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="<?php echo BASE_URL; ?>/img/ico/apple-touch-icon-144.png">
+      <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+      <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="js/respond.min.js"></script>
+      <![endif]-->
+      <!--[if IE]>
+      <link rel="stylesheet" href="css/ie.css">
+      <![endif]-->
+   </head>
+   <body>
+        <div class="container" style="min-height:850px;">
+            <div class="row">
+                <!-- Logo Start -->
+                <div class="logo pull-left">
+                    <a href="<?php echo BASE_URL; ?>">
+                        <img src="<?php echo BASE_URL; ?>/img/005-examhawk-logo.png" alt="ExamHawk">
+                    </a>
+               </div>
+               <!-- Logo End -->
+            </div>
+            <div class="divider"></div>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <h2>Administrator table of contents</h2>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-alexa-graphs.php">Alexa Graphs</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-alexa-metrics.php">Alexa Metrics</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-google-pagerank.php">Google PageRank</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-google-pagespeed-analysis.php">Google PageSpeed</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-google-serps.php">Google Serps</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-mozscape-metrics.php">Mozilla Metrics</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-opensiteexplorer-metrics.php">OpenSite Explorer Metrics</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-semrush-graphs.php">SEMrush Graphs</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-semrush-metrics.php">SEMrush Metrics</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-sistrix-visibilityindex.php">Sistrix Visibility</a><br>
+                    <a href="../seo-stats/SEOstats-dev-253/example/get-social-metrics.php">Social Metrics</a><br>
+                </div>
+             </div>
+        </div>
+        <!-- container end -->
+
+	   <!-- background image row end -->
+
+        <!-- Footer Start -->
+        <footer id="footer">
+           <!-- Footer Bottom Start -->
+           <div class="footer-bottom">
+              <div class="container">
+                 <div class="row">
+                    <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 text-center"> &copy; Copyright 2013 ExamHawk. All Rights Reserved. </div>
+                 </div>
+              </div>
+           </div>
+           <!-- Footer Bottom End -->
+        </footer>
+
+      <!-- The Scripts -->
+      <script src="<?php echo BASE_URL; ?>/js/jquery.min.js"></script>
+      <script src="<?php echo BASE_URL; ?>/js/bootstrap.js"></script>
+   </body>
+</html>
