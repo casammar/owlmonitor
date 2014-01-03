2d5aa0c035e748b4144b3591eca432017b069573
Chris <chris.sammarco@gmail.com>
2014-01-03 09:25:52 -0500

    correct path

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include 'views/baseurl.php'; ?>
+<?php include './views/baseurl.php'; ?>
 <!DOCTYPE html>
 <!--[if IE 8]>			<html class="ie ie8"> <![endif]-->
 <!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
