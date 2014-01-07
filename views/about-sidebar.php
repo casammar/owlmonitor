349eba88d4aa2c00f8bb1821b1cddaea7a365e44
Chris <chris.sammarco@gmail.com>
2014-01-06 21:51:03 -0500

    updated sidebar, added active class on matching URI

--- views/about-sidebar.php
+++ views/about-sidebar.php
@@ -1,71 +1,11 @@
-
-    <!-- Left nav Widget Start -->
-    <div class="widget category" style="min-width: 400px;">
-       <ul class="category-list slide">
-          <li><a href="<?php echo BASE_URL; ?>/about/index.php">About</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/institutions/faq.php">Institution FAQs</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/test-takers/faq.php">Test Taker FAQs</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/about/team.php">Team</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/about/contact.php">Contact</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/about/conferences.php">Conferences</a></li>
-          <li><a href="<?php echo BASE_URL; ?>/about/news.php">News</a></li>
-       </ul>
+<!-- Sidebar -->
+    <div class="list-group">
+          <a class="list-group-item <?= ($_SERVER['REQUEST_URI'] == '/beta/about/index.php' ? 'active' : '' ); ?>" href="<?php echo BASE_URL; ?>/about/index.php">About</a>
+          <a class="list-group-item <?= ($_SERVER['REQUEST_URI'] == '/beta/institutions/faq.php' ? 'active' : '' ); ?>" href="<?php echo BASE_URL; ?>/institutions/faq.php">Institution FAQs</a>
+          <a class="list-group-item <?= ($_SERVER['REQUEST_URI'] == '/beta/test-takers/faq.php' ? 'active' : '' ); ?>" href="<?php echo BASE_URL; ?>/test-takers/faq.php">Test Taker FAQs</a>
+          <a class="list-group-item <?= ($_SERVER['REQUEST_URI'] == '/beta/about/team.php' ? 'active' : '' ); ?>" href="<?php echo BASE_URL; ?>/about/team.php">Team</a>
+          <a class="list-group-item <?= ($_SERVER['REQUEST_URI'] == '/beta/about/contact.php' ? 'active' : '' ); ?>" href="<?php echo BASE_URL; ?>/about/contact.php">Contact</a>
+          <a class="list-group-item <?= ($_SERVER['REQUEST_URI'] == '/beta/about/conferences.php' ? 'active' : '' ); ?>" href="<?php echo BASE_URL; ?>/about/conferences.php">Conferences</a>
+          <a class="list-group-item <?= ($_SERVER['REQUEST_URI'] == '/beta/about/news.php' ? 'active' : '' ); ?>" href="<?php echo BASE_URL; ?>/about/news.php">News</a>
     </div>
-    <!-- Left nav Widget End -->
-    <!-- Tab Start -->
-    <? if($_SERVER['REQUEST_URI'] == '/beta/about/news.php'){ ?>
-    <div class="widget tabs">
-       <div id="horizontal-tabs">
-          <ul class="tabs">
-             <li id="tab1" class="current">Popular</li>
-             <li id="tab2">Recent</li>
-             <li id="tab3">Comments</li>
-          </ul>
-          <div class="contents">
-             <div class="tabscontent" id="content1" style="display: block;">
-                <ul class="posts">
-                   <li>
-                      <a href="#"><img class="img-thumbnail recent-post-img" alt="" src="http://placehold.it/68"></a>
-                      <p><a href="#">Lorem Ipsum is simply dummy text.</a></p>
-                      <span class="color">27 July 2013</span>
-                   </li>
-                   <li>
-                      <a href="#"><img class="img-thumbnail recent-post-img" alt="" src="http://placehold.it/68"></a>
-                      <p><a href="#">Lorem Ipsum is simply dummy text.</a></p>
-                      <span class="color">30 July 2013</span>
-                   </li>
-                </ul>
-             </div>
-             <div class="tabscontent" id="content2">
-                <ul class="posts">
-                   <li>
-                      <a href="#"><img class="img-thumbnail recent-post-img" alt="" src="http://placehold.it/68"></a>
-                      <p><a href="#">Lorem Ipsum is simply dummy text.</a></p>
-                      <span class="color">27 July 2013</span>
-                   </li>
-                   <li>
-                      <a href="#"><img class="img-thumbnail recent-post-img" alt="" src="http://placehold.it/68"></a>
-                      <p><a href="#">Lorem Ipsum is simply dummy text.</a></p>
-                      <span class="color">30 July 2013</span>
-                   </li>
-                </ul>
-             </div>
-             <div class="tabscontent" id="content3">
-                <ul class="posts">
-                   <li>
-                      <a href="#"><img class="img-thumbnail recent-post-img" alt="" src="http://placehold.it/68"></a>
-                      <p><a href="#">Lorem Ipsum is simply dummy text.</a></p>
-                      by <span class="color">wptuts+</span>
-                   </li>
-                   <li>
-                      <a href="#"><img class="img-thumbnail recent-post-img" alt="" src="http://placehold.it/68"></a>
-                      <p><a href="#">Lorem Ipsum is simply dummy text.</a></p>
-                      by <span class="color">wptuts+</span>
-                   </li>
-                </ul>
-             </div>
-          </div>
-       </div>
-    </div>
-     <? } ?>
-    <!-- Tab End -->
+<!-- Sidebar -->
